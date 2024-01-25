105435ba88f564af2bbedc1e48b324b6ffae80ed
lee14k <kmkovach@mtu.edu>
2024-01-25 17:02:05 -0500

    basic menu stack draft

--- components/StackedMenu.jsx
+++ components/StackedMenu.jsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 const StackedMenu = () => {
     return (
         <div className="menu-container">
-            <Image src='/menuone.png' height={160} width={100} className="menu-icon" />
+            <div className="menu-stack menu-one"></div>
             <div className="menu-stack"></div>
             <div className="menu-stack"></div>
-            <div className="menu-stack"></div>
+
         </div>
     );
 };
