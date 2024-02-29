ee7607e46791ed11fbe7c2ed918a8eba7c939bbc
lee14k <kmkovach@mtu.edu>
2024-02-29 01:19:34 -0500

    photorow bg and font

--- components/PhotoRow.jsx
+++ components/PhotoRow.jsx
@@ -1,43 +1,44 @@
-import Image from "next/image"
+import Image from "next/image";
 import { Rubik_Dirt } from "next/font/google";
 const rubik = Rubik_Dirt({
-    weight: '400',
-    subsets: ["latin"]
+  weight: "400",
+  subsets: ["latin"],
 });
+import { Merriweather } from "next/font/google";
 
+const merriweather = Merriweather({ weight: "400", subsets: ["latin"] });
 const PhotoRow = () => {
-    return (
-        <div className="headerbg flex flex-col justify-center items-center gap-4 py-24">
-            <div className="grid grid-col-1 sm:grid-cols-3 gap-4">
-                <div className="text-4xl flex flex-col justify-center items-center font-bold text-white">
-                    <Image
-                        src="/BBQ-.jpg"
-                        height={400}
-                        width={400}
-                        className="rounded"
-                    />
-                    <h4 className={rubik.className}>Hand Smoked BBQ</h4>
-                </div>
-                <div className="text-4xl flex flex-col justify-center items-center font-bold text-white">
-                    <Image
-                        src="/bfast.jpg"
-                        height={400}
-                        width={400}
-                        className="rounded"
-                    />
-                    <h4 className={rubik.className}>All Day Breakfast</h4>    </div>
-                <div className="text-4xl flex flex-col justify-center items-center font-bold text-white">
-                    <Image
-                        src="/lunch.jpg"
-                        height={400}
-                        width={400}
-                        className="rounded"
-                    />
-                    <h4 className={rubik.className}>Lunch & Other Bites</h4>    </div>
-            </div>
-            <button className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">View Menu</button>
+  return (
+    <div className="headerbg flex flex-col justify-center items-center gap-4 py-24">
+      <div className="grid grid-col-1 sm:grid-cols-3 gap-4">
+        <div className="text-4xl flex flex-col justify-center items-center font-bold ">
+          <Image src="/BBQ-.jpg" height={400} width={400} className="rounded" />
+          <h4 className={merriweather.className}>Hand Smoked BBQ</h4>
         </div>
-    )
-}
+        <div className="text-4xl flex flex-col justify-center items-center font-bold ">
+          <Image
+            src="/bfast.jpg"
+            height={400}
+            width={400}
+            className="rounded"
+          />
+          <h4 className={merriweather.className}>All Day Breakfast</h4>{" "}
+        </div>
+        <div className="text-4xl flex flex-col justify-center items-center font-bold ">
+          <Image
+            src="/lunch.jpg"
+            height={400}
+            width={400}
+            className="rounded"
+          />
+          <h4 className={merriweather.className}>Lunch & Other Bites</h4>{" "}
+        </div>
+      </div>
+      <button className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        View Menu
+      </button>
+    </div>
+  );
+};
 
-export default PhotoRow
+export default PhotoRow;
