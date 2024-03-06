fedbfe61802c57dda0b707997e96d696ce605e9b
lee14k <kmkovach@mtu.edu>
2024-03-06 17:01:01 -0500

    stuff on homepage

--- components/StackedMenu.jsx
+++ components/StackedMenu.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 const StackedMenu = () => {
     return (
+        <Link href="/menu">
         <div className="menu-container">
             <div className="menu-stack menu-one"></div>
             <div className="menu-stack"></div>
             
         </div>
+        </Link>
     );
 };
 
