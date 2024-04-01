b0a65c84d51c6998f54ce02d8c9a3cd98ecca301
lee14k <kmkovach@mtu.edu>
2024-04-01 19:22:39 -0400

    adding bbq menu, fixing prices to new menu

---
+++ components/BBQMenu.jsx
@@ -0,0 +1,259 @@
+const BBQMenu = () => {
+    const bbqone = [
+      {
+        header: "Wisconsin Cheese Curds",
+        body: "",
+        price: "$7.99",
+      },
+      {
+        header: "Deep Fried Loaded Potato Skins",
+        body: "",
+        price: "$8.59",
+      },
+      {
+        header: "Pretzel Nuggets & Beer Cheese",
+        body: "",
+        price: "$8.69",
+      },
+      {
+        header: "Onion Rings",
+        body: "",
+        price: "$7.99",
+      },
+      {
+        header: "Jalapeno Poppers",
+        body: "",
+        price: "$8.79",
+      },
+      {
+        header: "Fried Green Tomatoes",
+        body: "",
+        price: "$7.99",
+      },
+      {
+        header: "Handmade Chips & Cheese",
+        body: "",
+        price: "$7.99",
+      },
+      {
+        header: "5 Smoked Wings",
+        body: "",
+        price: "$8.99",
+      },
+      {
+        header: "10 Smoked Wings",
+        body: "",
+        price: "$12.99",
+      },
+      {
+        header: "Cup of Soup or Chili",
+        body: "",
+        price: "$4.99",
+      },
+      {
+        header: "Bowl of Soup or Chili",
+        body: "",
+        price: "$5.99",
+      },
+    ];
+  
+    const bbqtwo = [
+        {
+            header: "Loaded Smoked Mac & Cheese",
+            body: "Load your smoked mac & cheese with your choice of brisket or pulled pork topped with onion tanglers for a little crunch!",
+            price: "$14.99",
+          },
+          {
+            header: "Half Rack of Ribs",
+            body: "Slow smoked & served with sauce of the week and your choice of two sides!",
+            price: "$18.99",
+          },
+          {
+            header: "Full Rack of Ribs",
+            body: "Slow smoked & served with sauce of the week and your choice of two sides!",
+            price: "$24.99",
+          },
+          {
+            header: "Farmers Skillet",
+            body: "Brisket, pork, or chicken, served on deep fried American fries or crispy French fries, bacon bits, and cheese, side of source cream, and drizzle of sauce of thw eeek, side of Texas garlic toast, and side salad.",
+            price: "$14.99",
+          },
+          {
+            header: "Loaded Baked Potato or Fries",
+            body: "Top your potato with either brisket, pork, or smoked chicken, bacon, cheese, sour cream, and onion tanglers",
+            price: "$14.99",
+          },
+    ];
+    const bbqthree=[
+        {
+            header: "Nachos",
+            body: "Brisket, pork, or chicken on a bed of housemade chips and lettuce, topped with tomato, red, yellow, and green sweet peppers, red onion, sour cream, and salsa",
+            price: "$14.99",
+          },
+          {
+            header: "Burrito",
+            body: "Your choice of brisket, pulled pork, or smoked chicken in a twelve inch flour tortilla with cheese, refried beans, red onion, sour cream, and salsa",
+            price: "$12.99",
+          },
+          {
+            header: "Quesadilla",
+            body: "Brisket, pork, or smoked chicken with cheese and mixed peppers and onions",
+            price: "$12.99",
+          },
+          {
+            header: "Smoked Chicken Wrap",
+            body: "Mango tequila jalapeno sauce (not spicy!), cheese, lettuce, tomato, and side of fries",
+            price: "$15.99",
+          },
+
+    ];
+ const bbqburgers=[
+    {
+        header: "Taco Burger",
+        body: "Burger topped with Doritos, cheese sauce, and taco seasoning",
+        price: "$14.99",
+      },
+      {
+        header: "Barnyard",
+        body: "Burger topped with brisket or pork, cheese, lettuce, tomato, and red onion",
+        price: "$16.99",
+      },
+      {
+        header: "Haystack",
+        body: "Cheese, onion tanglers, slice of sweet red onion, and a sunny side up egg.",
+        price: "$16.99",
+      },
+      {
+        header: "Cowboy",
+        body: "Burger topped with onion rings, BBQ sauce, bacon, and cheddar jack cheese",
+        price: "$14.99",
+      },
+      {
+        header: "Hawaiian Chicken",
+        body: "Grilled BBQ chicken, grilled pineapple ring, lettuce, and tomato",
+        price: "$13.99",
+      },
+      {
+        header: "Hot Shot",
+        body: "Burger, bacon, pepper jack cheese, tomato, onion tanglers, and habanero bacon jam",
+        price: "$14.99",
+      },
+      {
+        header: "Just a Burger",
+        body: "1/3 lb burger patty with lettuce, tomato, onion (add cheese for $1.00 more)",
+        price: "$10.99",
+      },
+      {
+        header: "Four Piece Chicken Tender Basket",
+        body: "Four chicken tenders, dipping sauce, and fries",
+        price: "$11.99",
+      },
+      {
+        header: "Two Piece Chicken Tender Basket",
+        body: "Two chicken tenders, dipping sauce, and fries",
+        price: "$9.99",
+      },
+ ]
+
+ const pack =[
+    {
+        header: "One Meat, Two Sides, & Cornbread",
+        body: "",
+        price: "$18.99",
+      },
+      {
+        header: "Two Meats, Two Sides, & Cornbread",
+        body: "",
+        price: "$24.99",
+      },
+      {
+        header: "Backyard Sampler - all of the featured meats & 3 sides",
+        body: "",
+        price: "$38.99",
+      },
+ ]
+
+ const backwoods =[
+    {
+        header: "Pastrami Reuben",
+        body: "Tender slow smoked pastrami, melted swiss cheese, fresh sauerkraut, or cole slaw, and thousand island on grilled dark rye bread",
+        price: "$15.49",
+      },
+      {
+        header: "Smoked Hammy Sammy",
+        body: "Doubled smoked brown sugar ham, swiss cheese, and mayo on homemade white, wheat, or rye bread",
+        price: "$12.99",
+      },
+      {
+        header: "Smoked Turkey and Cranberry",
+        body: "Slow smoked turkey, your choice of cheese, on your choice of bread with cranberry sauce on the side",
+        price: "$14.99",
+      },
+      {
+        header: "Smokestack Sandwich",
+        body: "Smoked turkey and ham, bacon, lettuce, tomato, onion, swiss, and American cheese on your choice of homemade bread",
+        price: "$16.49",
+      },
+ ]
+  
+ const bbqdessert =[
+    {
+        header: "Slice of Pie or Smoked Brownie",
+        body: "",
+        price: "$4.99",
+      },
+      
+ ]
+    return (
+      <div>
+        <h1 className="text-6xl">Breakfast</h1>
+        <div className="grid grid-cols-3 gap-12">
+          {bfastone.map((bfastone, index) => (
+            <div key={index} className="grid">
+              <div>
+                <h2 className="text-2xl">{bfastone.header}</h2>
+              </div>
+              <div>
+                <p>{bfastone.body}</p>
+              </div>
+              <div>
+                <p>{bfastone.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <h1 className="text-4xl my-10">Cakes and French Toasts</h1>
+  
+        {bfasttwo.map((bfastone, index) => (
+          <div key={index} className="grid">
+            <div>
+              <h2 className="text-2xl">{bfastone.header}</h2>
+            </div>
+            <div>
+              <p>{bfastone.body}</p>
+            </div>
+            <div>
+              <p>{bfastone.price}</p>
+            </div>
+          </div>
+        ))}
+        <h2>Feed the Pack</h2>
+        <p>Choose from beef brisket, maple glazed pulled pork, BBQ pork ribs, smoked chicken, honey smoked ham, smoked turkey, or burnt ends</p>
+        {bfasttwo.map((bfastone, index) => (
+          <div key={index} className="grid">
+            <div>
+              <h2 className="text-2xl">{bfastone.header}</h2>
+            </div>
+            <div>
+              <p>{bfastone.body}</p>
+            </div>
+            <div>
+              <p>{bfastone.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  export default Breakfast;
+  
--- components/Breakfast.jsx
+++ components/Breakfast.jsx
@@ -3,62 +3,62 @@ const Breakfast = () => {
     {
       header: "Camp Special",
       body: "Two eggs, two sausage links, or two strips of bacon, hash browns, and toast.",
-      price: "10.49",
+      price: "$10.99",
     },
     {
       header: "CB Special",
       body: "Two eggs, Canadian bacon, and toast.",
-      price: "$8.29",
+      price: "$8.99",
     },
     {
       header: "Mini Special",
       body: "One egg, one sausage link or strip of bacon, half order of hash browns, and toast.",
-      price: "5.99",
+      price: "$6.99",
     },
     {
       header: "Swirl Special",
       body: "Two cinnamon swirl French toast, whipped butter and Canadian bacon.",
-      price: "8.99",
+      price: "$9.99",
     },
     {
       header: "Steak Special",
-      body: "Two eggs,  oz. sirloin strip steak, hash browns, and toast.",
-      price: "6",
+      body: "Two eggs, 8 oz. sirloin strip steak, hash browns, and toast.",
+      price: "$15.99",
     },
     {
       header: "Sportsman's Special",
       body: "Two griddle cakes with whipped butter, one egg, and two sausage links.",
-      price: "6.99",
+      price: "$7.99",
     },
     {
       header: "Northwoods Special",
       body: "Two eggs, two sausage links, two strips of bacon, and a slice of ham. With hash browns and toast.",
-      price: "$13.99",
+      price: "$14.99",
     },
     {
       header: "Logger's Special",
       body: "Two slices of French toast with whipped butter and three sausage links.",
-      price: "8.79",
+      price: "$9.79",
     },
     {
       header: "Northern Eggs Benedict",
       body: "Buttermilk biscuit split and topped with two eggs, Canadian bacon, and homemade sausage gravy.",
-      price: "10.99",
+      price: "$12.99",
     },
     {
       header: "Hunter's Special",
       body: "Two eggs, two sausage patties, hash browns, and toast.",
-      price: "10.99",
+      price: "$11.99",
     },
     {
       header: "Breakfast Sandwich",
       body: "Grilled Canadian bacon or sausage patty, one egg, and melted cheese on a toasted English muffin. .",
-      price: "5.29",
+      price: "$6.29",
     },
     {
       header: "Breakfast Burrito",
       body: "Scrambled eggs, bacon, hash browns, and cheese wrapped in a warm tortilla.  Famous Rolls",
-      price: "9.89",
+      price: "$10.89",
     },
     {
       header: "Cinnamon Roll 4.99",
@@ -66,46 +66,48 @@ const Breakfast = () => {
       price: null,
     },
     {
-      header: "Pecan Roll (Sundays",
+      header: "Pecan Roll (Sundays only)",
       body: [],
       price: null,
     },
     {
       header: "The Legend Country Fried Steak",
       body: "Country fried steak golden brown served with two eggs, hash browns, and choice of toast.",
-      price: "13.99",
-    },
+      price: "$14.99",
+    },]
+
+    const omelettes=[
     {
       header: "Wood Cutter",
       body: "Ham, mushroom, onion, green pepper, hash browns, and cheese blend.",
-      price: "11.49",
-    },
-    {
-      header: "Build your own omelette",
-      body: "Starting with three eggs and our cheese blend.",
-      price: "8.79",
+      price: "$11.99",
     },
     {
       header: "Meat Lover's",
       body: "Ham, bacon. Canadian bacon, pork sausage, and cheese blend.",
-      price: "12.49",
+      price: "$11.49",
     },
     {
       header: "Vegetarian",
       body: "Tomato, onion, green pepper, mushroom, and cheese blend.",
-      price: "10.49",
+      price: "$10.99",
     },
     {
-      header: "ADD CHEESE  0.99 each",
+      header: "Build your own omelette",
+      body: "Starting with three eggs and our cheese blend.",
+      price: "$9.79",
+    },
+    {
+      header: "ADD CHEESE  $0.99 each",
       body: [
-        "Cheddar cheese blend. Mozzarella,",
-        "Swiss, American, Provolone,",
+        "Cheddar jack blend. Mozzarella,",
+        "Swiss, American,",
         "Pepper Jack",
       ],
       price: null,
     },
     {
-      header: "ADD VEGGIES 0.60 each",
+      header: "ADD VEGGIES $0.70 each",
       body: [
         "Green Pepper, Onion, Tomato,",
         "Mushroom, Jalape\u00f1o, Hash",
@@ -114,94 +116,190 @@ const Breakfast = () => {
       price: null,
     },
     {
-      header: "ADD MEATS 1.95 each",
+      header: "ADD MEATS $2.25 each",
       body: [
         "Bacon, Ham, Canadian Bacon,",
         "Sausage, Taco Meat, Fajita Chicken.",
       ],
       price: null,
     },
+  ]
+  const skillets=[
     {
       header: "Timberman's",
       body: "Grilled ham, hash browns, onion, and green pepper, topped with two eggs and smothered with cheese sauce. Served with toast.",
-      price: "11.99",
+      price: "$12.99",
     },
     {
       header: "Corned Beef Hash",
-      body: "oz. of grilled corned beef hash topped with two eggs. Served with toast.",
-      price: "8",
+      body: "8 oz. of grilled corned beef hash topped with two eggs. Served with toast.",
+      price: "$12.99",
     },
     {
       header: "Sausage Skillet",
       body: "Hash browns topped with two sausage patties and two eggs covered with sausage gravy. Served with toast.",
-      price: "11.99",
+      price: "$12.99",
     },
     {
       header: "Polish Sausage Skillet",
       body: "Hearty skillet of polish sausage, crisp hash browns, green pepper and onion, topped with two eggs. Served with toast.",
-      price: "11.99",
+      price: "$12.99",
     },
-
+  ]
+  const everyday=[
     {
       header: "Biscuits and Gravy",
-      body: "Two homemade buttermilk biscuits, split, grilled, and topped with homemade sausage gravy. . | Half Order",
-      price: "9.89",
+      body: "Two homemade buttermilk biscuits, split, grilled, and topped with homemade sausage gravy.",
+      price: "$10.89 | Half Order $8.79 ",
     },
     {
       header: "Oatmeal or Cream of Wheat",
-      body: "Served with brown sugar and milk. Add raisins  Cup  | Bowl .",
-      price: "0.60",
+      body: "Served with brown sugar and milk. Add raisins $0.75 ",
+      price: "Cup $3.59 | Bowl 4.29",
     },
     {
-      header: "Egg and Toast 3.29",
-      body: "Add your choice ham, bacon, Canadian bacon, sausage links, or sausage patties.  * Two eggs for  more",
-      price: "7.29",
-    },
-    {
-      header: "Scrambled Egg Beaters",
-      body: "With toast.  With your choice ham, bacon, canadian bacon, sausage links, or sausage patties. ",
-      price: "6.59",
+      header: "Egg and Toast",
+      body: "Add your choice ham, bacon, Canadian bacon, sausage links, or sausage patties.  * Two eggs for $1.29 more",
+      price: "$7.59",
     },
   ];
 
   const bfasttwo = [
     {
-      header: "Two Griddle Cakes 4.39",
+      header: "Two Griddle Cakes $4.79",
       body: "With your choice ham, bacon, canadian bacon, sausage links, or sausage patties.",
-      price: "8.39",
+      price: "$8.79",
     },
     {
-      header: "Three Cinnamon Swirl",
-      body: [],
-      price: null,
+      header: "Three Cinnamon Swirl $8.99",
+      body: "With your choice ham, bacon, canadian bacon, sausage links, or sausage patties.",
+      price: "$12.99",
     },
     {
-      header: "French Toast 8.79",
-      body: [],
-      price: null,
+      header: "Three Griddle Cakes $5.89",
+      body: "With your choice ham, bacon, canadian bacon, sausage links, or sausage patties.",
+      price: "$9.89",
     },
     {
-      header: "Three Griddle Cakes 5.49",
-      body: [],
-      price: null,
+      header: "Three Blueberry Cakes $8.79",
+      body: "With your choice ham, bacon, canadian bacon, sausage links, or sausage patties.",
+      price: "$12.99",
     },
     {
-      header: "Three Blueberry Cakes 8.79",
-      body: [],
-      price: null,
-    },
-    {
-      header: "Three French Toast 7.69",
-      body: [],
-      price: null,
+      header: "Three French Toast $7.69",
+      body: "With your choice ham, bacon, canadian bacon, sausage links, or sausage patties.",
+      price: "$11.99",
     },
     {
       header: "Two Pearl Sugar Waffles",
-      body: "With syrup and butter.",
-      price: "8.29",
+      body: "With syrup and butter",
+      price: "$8.99",
     },
   ];
 
+const bsides = [
+  {
+    header: "Meats",
+    body: "Ham, Canadian bacon, bacon, sausage patty, or sausage link",
+    price: "$3.99",
+  },
+  {
+    header: "Sausage Gravy",
+    body: "",
+    price: "$2.99",
+  },
+  {
+    header: "Hash Browns or American Fries",
+    body: "",
+    price: "$2.99",
+  },
+  {
+    header: "Sweet Roll",
+    body: "",
+    price: "$5.99",
+  },
+  {
+    header: "Cinnamon Swirl Toast",
+    body: "",
+    price: "$2.29",
+  },
+  {
+    header: "English Muffin or Biscuit",
+    body: "",
+    price: "$2.29",
+  },
+  {
+    header: "Homemade Toast",
+    body: "",
+    price: "$2.29",
+  },
+]
+
+const sdrinks=[
+  {
+    header: "Coke, Diet Coke, Mello Yello, Sprite, Cherry Coke, Dr. Pepper",
+    body: "",
+    price: "$2.79",
+  },
+  {
+    header: "Fresh Brewed Iced Tea",
+    body: "Plain or Raspberry",
+    price: "$2.79",
+  },
+  {
+    header: "Lemonade",
+    body: "",
+    price: "$2.79",
+  },
+  {
+    header: "Apple, Cranberry, Orange, Tomato",
+    body: "",
+    price: "$2.79",
+  },
+  {
+    header: "Coffee",
+    body: "Fresh Brewed Classic Roast",
+    price: "$2.79",
+  },
+  {
+    header: "Hot Tea",
+    body: "",
+    price: "$2.20",
+  },
+  {
+    header: "Hot Chocolate",
+    body: "",
+    price: "$2.89",
+  },
+]
+
+const kids=[
+  {
+    header: "Two Pearl Sugar Waffles",
+    body: "With syrup and butter",
+    price: "$8.99",
+  },
+  {
+    header: "Two Pearl Sugar Waffles",
+    body: "With syrup and butter",
+    price: "$8.99",
+  },
+  {
+    header: "Two Pearl Sugar Waffles",
+    body: "With syrup and butter",
+    price: "$8.99",
+  },
+  {
+    header: "Two Pearl Sugar Waffles",
+    body: "With syrup and butter",
+    price: "$8.99",
+  },
+  {
+    header: "Two Pearl Sugar Waffles",
+    body: "With syrup and butter",
+    price: "$8.99",
+  },
+]
   return (
     <div>
       <h1 className="text-6xl">Breakfast</h1>
